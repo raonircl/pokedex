a501e943e5e9479756eb2711ab802bbef33846b5
raonircl <raoni.afi@gmail.com>
2021-08-14 23:23:50 -0300

    atualização de api e código js

--- script/script.js
+++ script/script.js
@@ -1,20 +1,27 @@
 const pokeUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-const generatePokemonPromises = () => Array(150).fill().map((_,index) => 
+const generatePokemonPromises = () => Array(890).fill().map((_,index) => 
     fetch(pokeUrl(index + 1)).then(reponse => reponse.json()))
-
+     
     const generateHTML = pokemons => pokemons.reduce((accumulator, {name, id, types}) => {
             const elementTypes = types.map(typeInfo => typeInfo.type.name);
-
+            if (id < 10) {
+                id2 = "00";
+            }else if (id > 9 && id <100) {
+                id2 = "0"
+            }else {
+                id2 = "";
+            }
             accumulator += `
                 <li class="card ${elementTypes[0]}">
-                <img class="card-image" alt="${name}" src="http://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
+                <img class="card-image" alt="${name}" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full//${id2+id}.png"/>
                 <h2 class="card-title">${id}.${name}</h2>
                 <p class="card-subtitle">${elementTypes.join(" | ")}</p>
                 </li> `
 
             return accumulator
-        }, "")
+            
+        }, "") 
     
     const insertPokemonIntopage = pokemons => {
         const ul = document.querySelector('[data-js="pokedex"]')
